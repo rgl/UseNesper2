a8bf50cd756e53b990064b80344e2b5044baf9bd
Rui Lopes <rui-lopes@telecom.pt>
2015-11-25 17:54:18 +0000

    Use xUnit.

---
+++ UseNEsper2/EplTests.cs
@@ -0,0 +1,194 @@
+﻿using com.espertech.esper.client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Xunit;
+
+namespace UseNEsper2
+{
+    public class ServiceEvent
+    {
+        public string Service { get; set; }
+    }
+
+    public class EplTests
+    {
+        [Fact]
+        public void GroupByRegression()
+        {
+            string expected;
+            string resultProperties;
+            var results = new List<EventBean[]>();
+
+            using (var engine = CreateEngine())
+            {
+                engine.EPAdministrator.Configuration.AddEventType("Service", typeof(ServiceEvent));
+
+                var statement = engine.EPAdministrator.CreateEPL(@"
+                    select Service, count(*) as Value
+                    from Service.win:time_batch(1 seconds)
+	                group by Service
+                    order by Service
+                ");
+
+                resultProperties = GetStatementProperties(statement);
+
+                expected = @"
+                    Service:String
+                    Value:Int64
+
+                    #0.0: Service:(null)=(null) Value:Int64=3
+                    #0.1: Service:String=Test Value:Int64=7
+
+                    #1.0: Service:(null)=(null) Value:Int64=3
+                    #1.1: Service:String=Test Value:Int64=7
+
+                    #2.0: Service:(null)=(null) Value:Int64=0
+                    #2.1: Service:String=Test Value:Int64=0
+                ";
+
+                statement.Events += (sender, args) => results.Add(args.NewEvents);
+
+                for (var n = 0; n < 10; ++n)
+                {
+                    engine.EPRuntime.SendEvent(
+                        new ServiceEvent
+                        {
+                            Service = n % 4 == 0 ? null : "Test"
+                        }
+                    );
+                }
+
+                Thread.Sleep(TimeSpan.FromSeconds(1.5));
+
+                for (var n = 0; n < 10; ++n)
+                {
+                    engine.EPRuntime.SendEvent(
+                        new ServiceEvent
+                        {
+                            Service = n % 4 == 0 ? null : "Test"
+                        }
+                    );
+                }
+
+                Thread.Sleep(TimeSpan.FromSeconds(2.5));
+            }
+
+            var result = string.Format("{0}\n\n{1}", resultProperties, ResultsToString(results));
+
+            Assert.Equal(FormatResult(expected), result);
+        }
+
+        private static EPServiceProvider CreateEngine()
+        {
+            var configuration = new Configuration();
+
+            configuration.AddEventType<ServiceEvent>("Service");
+
+            var engine = EPServiceProviderManager.GetProvider("Engine", configuration);
+
+            return engine;
+        }
+
+        private static string GetStatementProperties(EPStatement statement)
+        {
+            return string.Join(
+                "\n",
+                statement
+                    .EventType
+                    .PropertyDescriptors
+                    .Select(d => string.Format("{0}:{1}", d.PropertyName, GetStatementPropertyType(d.PropertyName, d.PropertyType)))
+            );
+        }
+
+
+        public static string GetStatementPropertyType(string propertyName, Type propertyType)
+        {
+            if (propertyType.IsGenericType)
+            {
+                // if it's a Nullable we really want to get the type of the type wrapped by the nullable.
+                var underlyingType = Nullable.GetUnderlyingType(propertyType);
+
+                if (underlyingType != null)
+                {
+                    return GetPropertyType(underlyingType);
+                }
+
+                // if it's a ICollection (or its base interface IEnumerable) return it as Array
+                if (propertyType.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEnumerable<>)))
+                {
+                    return "Array";
+                }
+
+                throw new ArgumentOutOfRangeException(string.Format("the property {0} has the unsupported property type {1}", propertyName, propertyType));
+            }
+            else
+            {
+                return GetPropertyType(propertyType);
+            }
+        }
+
+        private static string GetPropertyType(Type type)
+        {
+            if (type.IsEnum)
+                return "String";
+
+            if (type.IsArray)
+                return "Array";
+
+            var typeName = type.Name;
+
+            switch (typeName)
+            {
+                case "Boolean":
+                case "DateTime":
+                case "Double":
+                case "Int32":
+                case "Int64":
+                case "String":
+                    return typeName;
+
+                default:
+                    return "Object";
+            }
+        }
+
+        private static string FormatResult(string s)
+        {
+            return Regex.Replace(Regex.Replace(s, @"^[ \t]+", "", RegexOptions.Multiline).Trim(), @"\r", "");
+        }
+
+        private static string ResultsToString(IEnumerable<EventBean[]> results)
+        {
+            return string.Join("\n\n", results.Select(ResultToString));
+        }
+
+        private static string ResultToString(EventBean[] result, int n)
+        {
+            return string.Join("\n", result.Select((r, i) => ResultToString(r, i, n)));
+        }
+
+        private static string ResultToString(EventBean result, int i, int n)
+        {
+            return string.Format(
+                "#{0}.{1}: {2}",
+                n,
+                i,
+                string.Join(
+                    " ",
+                    result.EventType.PropertyNames
+                        .Select(
+                            name =>
+                            {
+                                var value = result.Get(name);
+
+                                return string.Format("{0}:{1}={2}", name, value != null ? value.GetType().Name : "(null)", value ?? "(null)");
+                            }
+                        )
+                )
+            );
+        }
+    }
+}
--- UseNEsper2/Program.cs
+++ UseNEsper2/Program.cs
@@ -1,7 +1,5 @@
-﻿using com.espertech.esper.client;
-using System;
-using System.Text;
-using System.Threading;
+﻿using System;
+using Xunit.Sdk;
 
 namespace UseNEsper2
 {
@@ -9,110 +7,18 @@ namespace UseNEsper2
     {
         static void Main(string[] args)
         {
-            var configuration = new Configuration();
+            // execute the tests with the poors' man runner...
 
-            //configuration.AddPlugInSingleRowFunction("current_time", typeof(Time).FullName, "Current");
+            var tests = new EplTests();
 
-            configuration.AddEventType<ServiceEvent>("Service");
-
-            var engine = EPServiceProviderManager.GetProvider("Engine", configuration);
-
-            var allStatement = StartStatement(
-                engine,
-                "All",
-                @"
-                    select
-                        //current_time() as Time,
-                        Service,
-                        Service is null as ServiceIsNull
-                    from
-                        Service.win:time_batch(5 seconds)
-                "
-            );
-
-            var groupsStatement = StartStatement(
-                engine,
-                "Groups",
-                @"
-                    select
-                        //current_time() as Time,
-                        Service,
-                        Service is null as IsServiceNull,
-                        count(*) as Services,
-                        count(Service is null) as NullServices,
-                        count(Service is not null) as NotNullServices
-                    from
-                        Service.win:time_batch(5 seconds)
-                    group by
-                        Service
-                "
-            );
-
-            var eventSender = engine.EPRuntime.GetEventSender("Service");
-
-            for (var n = 0; !Console.KeyAvailable; ++n)
+            try
             {
-                var serviceEvent = new ServiceEvent
-                {
-                    Service = n % 2 == 0 ? "Service"+n : null,
-                };
-
-                eventSender.SendEvent(serviceEvent);
-
-                if (n%10 == 0)
-                {
-                    Console.WriteLine("{0} Sent {1} events", DateTime.Now, n);
-                }
-
-                Thread.Sleep(500);
+                tests.GroupByRegression();
+            }
+            catch (XunitException e)
+            {
+                Console.WriteLine(e);
             }
         }
-
-        private static EPStatement StartStatement(EPServiceProvider engine, string statementName, string epl)
-        {
-            var statement = engine.EPAdministrator.CreateEPL(epl);
-
-            statement.Events += (sender, e) =>
-            {
-                var result = new StringBuilder();
-                result.AppendFormat("{0} {1} Batch:\n", DateTime.Now, statementName);
-                DumpEvents("OldEvents", e.OldEvents, result);
-                DumpEvents("NewEvents", e.NewEvents, result);
-                Console.WriteLine(result.ToString());
-            };
-
-            return statement;
-        }
-
-        private static void DumpEvents(string title, EventBean[] e, StringBuilder result)
-        {
-            if (e == null)
-                return;
-
-            result.AppendFormat("  {0}:\n", title);
-
-            foreach (var bean in e)
-            {
-                foreach (var name in bean.EventType.PropertyNames)
-                {
-                    result.AppendFormat("    {0}={1}", name, bean.Get(name) ?? "(null)");
-                }
-
-                result.AppendLine();
-            }
-        }
-    }
-
-    public class Time
-    {
-        public static DateTime Current()
-        {
-            return DateTime.UtcNow;
-        }
-    }
-
-    public class ServiceEvent
-    {
-        public string Service { get; set; }
     }
 }
