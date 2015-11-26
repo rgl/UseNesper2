19cef7495a229b967d70f20d5872ecf48e306ee7
Rui Lopes <rui-lopes@telecom.pt>
2015-11-26 15:57:17 +0000

    Add Java version. The `group by` statement works fine with the latest esper v5.3.

---
+++ java/src/main/java/com/example/ServiceEvent.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class ServiceEvent {
+    private final String service;
+
+    public ServiceEvent(String service) {
+        this.service = service;
+    }
+    
+    public String getService() {
+        return service;
+    }
+}
